b66bef2fe6e9278596dd5dac10d8eb942da64867
Vanessa Alves <vanessa.182@hotmail.com>
2018-04-19 00:15:07 -0300

    Vídeo e background overlay

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,8 @@ export default class Main extends React.Component {
                 <h1 className="home-title">StarQuiz!</h1>
                 <p className="home-txt">Play now and test your knowledge in Star Wars</p>
                 <a href="/game" className="home-start">Start!</a>
+                <div className="home-overlay"></div>
+                <video className="home-clip" loop autoPlay={true} muted poster="/static/img/bg/sw2.jpg" src="/static/videos/clip.mp4"></video>
             </section>
         );
     }
